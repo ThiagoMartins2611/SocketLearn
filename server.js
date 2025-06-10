127b73fae5a35a2922a3d69ae31dfa90a9f6ecc0
RPGTStudios <174332720+ThiagoMartins2611@users.noreply.github.com>
2025-06-09 22:59:14 -0400

    Pasta Html adicionada, instaciamento do socket.io e connect e disconnect do usuario

---
+++ server.js
@@ -0,0 +1,36 @@
+
+//https://socket.io/docs/v4/tutorial/step-4 *acessa esse site Thiago ele vai te ensinar o caminho*
+
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server)
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+app.get('/', (req, res)=>{
+    
+    res.sendFile(join(__dirname,'public', 'index.html'));
+
+});
+
+
+io.on('connection', (socket)=>{
+    console.log("usuario conectado");
+
+    socket.on('disconnect', ()=>{
+        console.log("usuario disconectado")
+    });
+});
+
+
+server.listen(7000, ()=>{
+    console.log("o servidor está rodadndo na porta: 7000, http://localhost:7000")
+})
